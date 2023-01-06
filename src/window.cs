9697ab1216d2014eb94753030a992f14c3c64e23
vaporvee <yannik@vaporvee.com>
2023-01-06 16:53:45 +0100

    jitterfix 0.5f faster

--- src/window.cs
+++ src/window.cs
@@ -8,7 +8,7 @@ public partial class window : Node
         /* VSync should be used because needing to change the PhysicsTicksPerSecond in _Process takes CPU ussage
          * You could also just set max FPS and delte this fix
          */
-        await ToSignal(GetTree().CreateTimer(2f), "timeout");//waits until the game has loaded some time
+        await ToSignal(GetTree().CreateTimer(1.5f), "timeout");//waits until the game has loaded some time
         if (Engine.PhysicsTicksPerSecond != (int)Engine.GetFramesPerSecond())
         {
             Engine.PhysicsTicksPerSecond = (int)Engine.GetFramesPerSecond(); //PhysicsTicksPerSecond have to be the same value like current FPS or the movement will lagg
