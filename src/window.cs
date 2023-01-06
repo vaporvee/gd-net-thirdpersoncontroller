eee59dfa6abfe524254cae933572d84a61677ce7
vaporvee <yannik@vaporvee.com>
2023-01-06 16:49:36 +0100

    custom actual working jitterfix

--- src/window.cs
+++ src/window.cs
@@ -3,12 +3,19 @@ using System;
 
 public partial class window : Node
 {
-	public override void _Ready()
-	{
-        ProjectSettings.SetSetting("physics/common/physics_ticks_per_second", Performance.GetMonitor(Performance.Monitor.TimeFps));
-	}
-
-	public override void _Process(double delta)
+    public override async void _Ready()
+    {
+        /* VSync should be used because needing to change the PhysicsTicksPerSecond in _Process takes CPU ussage
+         * You could also just set max FPS and delte this fix
+         */
+        await ToSignal(GetTree().CreateTimer(2f), "timeout");//waits until the game has loaded some time
+        if (Engine.PhysicsTicksPerSecond != (int)Engine.GetFramesPerSecond())
+        {
+            Engine.PhysicsTicksPerSecond = (int)Engine.GetFramesPerSecond(); //PhysicsTicksPerSecond have to be the same value like current FPS or the movement will lagg
+            GD.Print("Set PhysicsTicksPerSecond to: " + Engine.PhysicsTicksPerSecond);
+        }
+    }
+    public override void _Process(double delta)
 	{
         if (Input.IsActionJustPressed("fullscreen"))
         {
